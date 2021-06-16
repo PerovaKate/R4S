e787847b1211fb7af4eab9d0e5b51c0d46c3b78e
PerovaKate <85638736+PerovaKate@users.noreply.github.com>
2021-06-16 20:29:59 +0300

    123

---
+++ HI_on_R4S!.py
@@ -0,0 +1,84 @@
+users = 5
+print(users)
+gender = "male"
+print(gender)
+height = 1.78
+print(height)
+weight = 91
+print(weight)
+date_of_birth = "14 july 1995"
+print(date_of_birth)
+fat = 15
+print(fat)
+muscle = 42
+print(muscle)
+bones = 20
+print(bones)
+imt1 = weight/height**2
+print(imt1)
+gender = "female"
+print(gender)
+height = 1.55
+print(height)
+weight = 57
+print(weight)
+date_of_birth = "29 september 1988"
+print(date_of_birth)
+fat = 20
+print(fat)
+muscle = 31
+print(muscle)
+bones = 15
+print(bones)
+imt2 = weight/height**2
+print(imt2)
+gender = "female"
+print(gender)
+height = 1.71
+print(height)
+weight = 66
+print(weight)
+date_of_birth = "9 may 2000"
+print(date_of_birth)
+fat = 10
+print(fat)
+muscle = 19
+print(muscle)
+bones = 20
+print(bones)
+imt3 = weight/height**2
+print(imt3)
+gender = "male"
+print(gender)
+height = 1.92
+print(height)
+weight = 99
+print(weight)
+date_of_birth = "27 february 1987"
+print(date_of_birth)
+fat = 21
+print(fat)
+muscle = 40
+print(muscle)
+bones = 40
+print(bones)
+imt4 = weight/height**2
+print(imt4)
+gender = "female"
+print(gender)
+height = 1.62
+print(height)
+weight = 45
+print(weight)
+date_of_birth = "8 june 1997"
+print(date_of_birth)
+fat = 11
+print(fat)
+muscle = 15
+print(muscle)
+bones = 10
+print(bones)
+imt5= weight/height**2
+print(imt5)
+
+
